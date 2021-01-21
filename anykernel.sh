33b9f51101a9d699108b83895e2f2de95eb2aa03
Aris Hidayatulloh <68738612+sssira@users.noreply.github.com>
2021-01-21 18:19:47 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Prooholic Kranul by Prooholic @telegram
+kernel.string=sssira Kernel by sssira @telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
